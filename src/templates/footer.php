d2d675494cd4319fe1f3060012c393e92c8214e1
Guilain De Meyer <yrautcnas@msn.com>
2024-11-02 18:01:57 +0100

    Car-Nation

---
+++ src/templates/footer.php
@@ -0,0 +1,14 @@
+<!-- FOOTER -->
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark">
+        <div class="nav col-12 col-lg-4 justify-content-center">
+            <p class="Footertxt">© 2024 Car-Nation, Inc</p>
+        </div>
+
+        <a href="index.php" class="col-12 col-lg-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <img src="Assets/Logo/Car-NationNoBG.png" alt="logoCN" width="80px">
+        </a>
+
+        <div class="nav col-12 col-lg-4 justify-content-center">
+            <p><a href="index.php" class="Footertxt nav-link px-2 text-body-active">Nous contacter</a></p>
+        </div>
+    </footer>
---
+++ src/templates/header.php
@@ -0,0 +1,21 @@
+<!-- HEADER -->
+<header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4 border-bottom navbar-dark bg-dark">
+        <div class="col-12 col-lg-4 justify-content-center">
+            <a href="index.php" class="d-flex align-items-center justify-content-center mb-3 mb-md-0 link-body-emphasis text-decoration-none">
+                <img src="Assets/Logo/Car-NationNoBG.png" alt="logoCN" width="80px">
+            </a>
+        </div>
+
+        <ul class="nav nav-pills col-12 col-lg-4 mb-2 justify-content-center align-items-center">
+            <li><a href="#" class="NavHeader nav-link active px-2">Présentation</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Galerie Photos</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Atelier</a></li>
+            <li><a href="recrutement.php" class="NavHeader nav-link px-2">Recrutement</a></li>
+            <li><a href="#" class="NavHeader nav-link px-2">Nous contacter</a></li>
+        </ul>
+
+        <div class="col-12 col-lg-4 d-flex justify-content-center">
+            <button type="button" class="btn btn-outline-danger me-2">Login</button>
+            <button type="button" class="btn btn-danger">Sign-up</button>
+        </div>
+    </header>
