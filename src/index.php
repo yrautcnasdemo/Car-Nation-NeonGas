73062a50c6b98b8d0e1ea309e04807db800669ec
Guilain De Meyer <yrautcnas@msn.com>
2025-01-31 17:33:29 +0100

    Neon-gas

--- src/index.php
+++ src/index.php
@@ -5,19 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Car-Nation</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="font.css">
 </head>
 <body>
 
 
 <main>
     <section class="main-section">
-
+        <figure>
+            <h1>Car-Nation : Bienvenue dans l'Atelier du Futur Passé</h1>
+            <p>Ici, nous ne réparons pas seulement des voitures, nous les préparons en cas d'apocalipse.
+                Depuis des années, nous redonnons vie à de pures merveilles des années passés mais pas révolu dans nos coeurs 
+                Nos véhicules ne sont pas seulement réhabilités pour dominer l’asphalte, mais ils sont prêts à affronter toutes les épreuves de la vie actuelle et à venir,
+                embouteillage parisien (les pires, ceux qui ferait abandonner un T1000), tempêtes radioactives (car les changements climatiques ne gênent pas que les pingouins) et
+                même les bombardements nucléaires (car contrairement aux frigos, nos voitures peuvent accueillir plus d’une personne).
+                Grâce à notre savoir-faire artisanal, nous transformons chaque voiture en monstre de métal qui ne craint rien ni personne, pas même Mr Wick.</p>
+                <figcaption>
+                    <img src="" alt="">
+                </figcaption>
+        </figure>
+                
     </section>
 
     <section>
-        <h1>zeig zipegjp izejgpi zejpgj epzgjpiez j</h1>
-        <p>afkh zpioegjoiez gizipeg jpezjg pezjgp jezpgj piezjg ezjpig jezi</p>
     </section>
+
+    
 </main>
 
 <script src="script.js"></script>
