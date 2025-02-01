4efeffc6da05f6dc8078c38312e03fddc6b03c5e
Guilain De Meyer <yrautcnas@msn.com>
2025-02-01 14:16:56 +0100

    synchro

--- src/index.php
+++ src/index.php
@@ -12,22 +12,24 @@
 
 <main>
     <section class="main-section">
+        
+    </section>
+        
+    <section class="second-section">
         <figure>
-            <h1>Car-Nation : Bienvenue dans l'Atelier du Futur Passé</h1>
-            <p>Ici, nous ne réparons pas seulement des voitures, nous les préparons en cas d'apocalipse.
-                Depuis des années, nous redonnons vie à de pures merveilles des années passés mais pas révolu dans nos coeurs 
-                Nos véhicules ne sont pas seulement réhabilités pour dominer l’asphalte, mais ils sont prêts à affronter toutes les épreuves de la vie actuelle et à venir,
-                embouteillage parisien (les pires, ceux qui ferait abandonner un T1000), tempêtes radioactives (car les changements climatiques ne gênent pas que les pingouins) et
-                même les bombardements nucléaires (car contrairement aux frigos, nos voitures peuvent accueillir plus d’une personne).
-                Grâce à notre savoir-faire artisanal, nous transformons chaque voiture en monstre de métal qui ne craint rien ni personne, pas même Mr Wick.</p>
+            <div class="intro">
+                <h1>Car-Nation : Bienvenue dans l'Atelier du Futur Passé</h1>
+                <p>Ici, nous ne réparons pas seulement des voitures, nous les préparons en cas d'apocalipse.
+                    Depuis des années, nous redonnons vie à de pures merveilles des années passés mais pas révolu dans nos coeurs 
+                    Nos véhicules ne sont pas seulement réhabilités pour dominer l’asphalte, mais ils sont prêts à affronter toutes les épreuves de la vie actuelle et à venir,
+                    embouteillage parisien (les pires, ceux qui ferait abandonner un T1000), tempêtes radioactives (car les changements climatiques ne gênent pas que les pingouins) et
+                    même les bombardements nucléaires (car contrairement aux frigos, nos voitures peuvent accueillir plus d’une personne).
+                    Grâce à notre savoir-faire artisanal, nous transformons chaque voiture en monstre de métal qui ne craint rien ni personne, pas même Mr Wick.</p>
+            </div>
                 <figcaption>
-                    <img src="" alt="">
+                    <img src="/Assets/Gallery/01-1971-plymouth-hemi-barracuda-front-three-quarter.jpg" alt="">
                 </figcaption>
         </figure>
-                
-    </section>
-
-    <section>
     </section>
 
     
